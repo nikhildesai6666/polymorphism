874123b7b64c04600a3cac000a7ad2ebc1fed91d
nikhildesai6666 <desainikhil9664@gmail.com>
2022-10-12 12:57:07 +0530

    Add project files.

---
+++ polymorphism/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class Class1
+    {
+        static void Main(string[]args)
+        {
+            Employee e1 = new Employee("Nikhil", 60000);
+            e1.CalculateSalary();
+            Console.WriteLine(e1);
+
+            Manager m1 = new Manager("Nikhil", 60000, 1500);
+            m1.CalculateSalary();
+            Console.WriteLine(m1);
+
+            Precident p1 = new Precident("Nikhil", 60000, 4521);
+            p1.CalculateSalary();
+            Console.WriteLine(p1);
+
+            SalesManager s1 = new SalesManager("Nikhil", 60000, 8976);
+            s1.CalculateSalary();
+            Console.WriteLine(s1);
+            
+            
+
+
+        }
+    }
+}
---
+++ polymorphism/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    public class Employee
+    {
+        protected int id;
+        protected string name;
+        protected double basicsalary, hra, da, pf, netpaid;
+        private static int count;
+        public Employee(string name, double basicsalary)
+        {
+            count++;
+            this.id = count;
+            this.name = name;
+            this.basicsalary = basicsalary;
+        }
+        public virtual void CalculateSalary()
+        {
+            hra = basicsalary * 0.40;
+            da = basicsalary * 0.20;
+            pf = basicsalary * 0.12;
+            netpaid = (hra + da + basicsalary) - pf;
+        }
+        public static string TotalCount()
+        {
+            return $"Total count is {count}";
+        }
+        public override string ToString()
+        {
+            return $"Employee details -->Id :{id} Name :{name} Salary {netpaid}";
+        }
+    }
+
+    public class Manager : Employee
+    {
+        private double ta;
+        public Manager(string name, double bs, double ta) : base(name, bs) 
+        {
+            this.ta = ta;
+        }
+        public override void CalculateSalary()
+        {
+            hra = basicsalary * 0.40;
+            da = basicsalary * 0.20;
+            pf = basicsalary * 0.12;
+            netpaid = (hra + da + basicsalary + ta) - pf;
+        }
+        public override string ToString()
+        {
+            return $"Manager details -->Id :{id} Name :{name} Salary {netpaid}";
+        }
+    }
+    public class Precident : Employee
+    {
+        private double bonas;
+        public Precident(string name, double bs,double bonus) : base(name,bs)
+        {
+            this.bonas = bonus;
+        }
+        public override void CalculateSalary()
+        {
+               hra = basicsalary * 0.40;
+               da = basicsalary * 0.20;
+               pf = basicsalary * 0.12;
+               netpaid = (hra + da + basicsalary + bonas) - pf;
+        }
+        public override string ToString()
+        {
+            return $"Precident details ---> Id : {id} Name : {name} Salary : {netpaid}"; 
+
+        }
+
+        
+    }
+    public class SalesManager : Employee
+    {
+        private double insurance;
+        public SalesManager(string name, double bs, double bonus) : base(name, bs)
+        {
+            this.insurance = insurance;
+        }
+        public override void CalculateSalary()
+        {
+            hra = basicsalary * 0.40;
+            da = basicsalary * 0.20;
+            pf = basicsalary * 0.12;
+            netpaid = (hra + da + basicsalary + insurance) - pf;
+        }
+        public override string ToString()
+        {
+            return $"Precident details ---> Id : {id} Name : {name} Salary : {netpaid}";
+
+        }
+
+
+
+    }
